6c7cfbd27892aaebde0e5a6aaa3e2060c7495c9a
--global <--global>
2025-05-21 20:24:56 -0500

    some better stuff

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def draw(images):
             plat2 = drawRect(window, (1910, 0), (10, 300), scale = scale, shift = shift, texture = os.path.join("images","dirt.jpg"), cache = images)
             plat3 = drawRect(window, (-100, 0), (10, 300), scale = scale, shift = shift, texture = os.path.join("images","dirt.jpg"), cache = images)
             plat4 = drawRect(window, (1900, 195), (10, 10), scale = scale, shift = shift, texture = os.path.join("images","dirt.jpg"), cache = images)
-            plat5 = drawRect(window, (-100,0), (2020, 10), scale = scale, shift = shift, texture = os.path.join("images","dirt.jpg"), cache = images)
+            plat5 = drawRect(window, (-100,-10), (2020, 10), scale = scale, shift = shift, texture = os.path.join("images","dirt.jpg"), cache = images)
             win = drawRect(window, (1920/2-25, 1080-70), (50,50), scale = scale, shift = shift, color = (255,255,0))
             for i in (floor, plat1, plat2, plat3, plat4, plat5):
                 if i.image:
@@ -299,6 +299,7 @@ while drawing:
         # Sound
         if not game_over:
             sound = pygame.mixer.Sound(os.path.join("sounds","vineboom.mp3"))
+            sound.set_volume(0.5)
             sound.play() 
         game_over_rect = death_font.get_rect("You died.")
         restart_rect = start_font.get_rect("Press space to restart.")
@@ -309,6 +310,7 @@ while drawing:
     if player.rect.colliderect(win.rect) or (keys[pygame.K_p] and keys[pygame.K_f] and keys[pygame.K_l]):
         if not skip:
             sound = pygame.mixer.Sound(os.path.join("sounds","cheer.mp3"))
+            sound.set_volume(0.5)
             sound.play() 
             player.x, player.y = (200,0)
             player.draw(window, scale)
